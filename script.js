ec060148b6257b56354add0f58c5c47e26b32d17
itsy bitsy <itsy_bitsy@itsy.bitsy>
2023-07-13 13:58:31 -0400

    Js calculates size of grid

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const section = document.querySelector('section');
 
 const container = document.createElement('div');
 container.setAttribute('id', 'grid');
+container.style.height = '500px';
+container.style.aspectRatio = 1 / 1;
 section.appendChild(container);
 
-const gridNumber = 16;
-for (i = 0; i < gridNumber; i++) {
+const gridNumber = 10;
+// const gridHeight = document.getElementById('grid').style.height;
+// console.log(gridHeight);
+for (i = 0; i < gridNumber **2; i++) {
         const row = document.createElement('div');
         row.style.border = 'thin dotted black';
+        row.style.height = parseInt(container.style.height) / gridNumber + 'px';
+        row.style.aspectRatio = 1 / 1;
+        console.log(row.style.height);
         row.setAttribute('id', 'row');
         container.appendChild(row);
     }
