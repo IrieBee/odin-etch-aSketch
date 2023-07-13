facaf73f7a9a377693837c09025bd064cd62ce56
itsy bitsy <itsy_bitsy@itsy.bitsy>
2023-07-13 18:01:07 -0400

    Change color mouseover for all grids

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const section = document.querySelector('section');
 
 const container = document.createElement('div');
 container.setAttribute('id', 'grid');
-container.style.height = '100px';
+container.style.height = '300px';
 container.style.aspectRatio = 1 / 1;
 section.appendChild(container);
 
-const gridNumber = 7;
+const gridNumber = 16;
 
 const gridHeight = parseInt(container.style.height) / gridNumber + 'px';
  console.log(gridHeight);
@@ -17,16 +17,19 @@ for (let i = 0; i < gridNumber **2; i++) {
     row.style.height = gridHeight;
     row.style.aspectRatio = 1 / 1;
     console.log(row.style.height);
-    row.setAttribute('id', 'row');
+    row.setAttribute('class', 'row');
     container.appendChild(row);
 
 }
 
-const drawing = document.querySelector('#row');
-drawing.addEventListener('mouseover', () => {
-    drawing.style.backgroundColor = "hsl(280, 10%, 25%)";
+// Add querySelectorAll
+const drawing = document.querySelectorAll('.row');
+console.log(drawing);
+drawing.forEach ((box) => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'hsl(280, 10%, 25%)';
     })
-
+})
 
 // for (i = 0; i < gridNumber; i++) {
 //     const grid = document.createElement('div');
