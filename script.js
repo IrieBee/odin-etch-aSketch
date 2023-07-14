629316b961a42e90b6347042944ae0964c005e4e
itsy bitsy <itsy_bitsy@itsy.bitsy>
2023-07-14 07:51:14 -0400

    Move style properties for grid from js to css

--- script.js
+++ script.js
@@ -2,32 +2,31 @@ const section = document.querySelector('section');
 
 const container = document.createElement('div');
 container.setAttribute('id', 'grid');
-container.style.height = '600px';
+container.style.height = '500px';
 container.style.aspectRatio = 1 / 1;
 section.appendChild(container);
 
 // Add prompt from user
-const gridNumber = 60;
+const gridNumber = 20;
 // const gridNumber = document.querySelector(.button);
 // gridNumber = addEventListener('click', () => {
 //     alert()
 // })
 
+
+// Create grid of small squares
 const gridHeight = parseInt(container.style.height) / gridNumber + 'px';
- console.log(gridHeight);
 let row = undefined;
 for (let i = 0; i < gridNumber **2; i++) {
     row = document.createElement('div');
-    row.style.border = 'thin dotted hsl(210, 5%, 85%)';
     row.style.height = gridHeight;
-    row.style.aspectRatio = 1 / 1;
+    // row.style.aspectRatio = 1 / 1;
     console.log(row.style.height);
     row.setAttribute('class', 'row');
     container.appendChild(row);
-
 }
 
-// Add querySelectorAll
+// Add querySelectorAll to draw on all grids
 const drawing = document.querySelectorAll('.row');
 console.log(drawing);
 drawing.forEach ((box) => {
@@ -36,9 +35,3 @@ drawing.forEach ((box) => {
     })
 })
 
-// for (i = 0; i < gridNumber; i++) {
-//     const grid = document.createElement('div');
-//     grid.style.border = 'thin dotted black';
-//     grid.setAttribute('id', 'column');
-//     container.appendChild(grid);
-// }
