bea5bc0c9e415686a897aae19b2166d8ec0435d0
itsy bitsy <119555750+ltsyBitsy@users.noreply.github.com>
2023-07-18 16:07:07 -0400

    Delete previous grid and make a new one

--- script.js
+++ script.js
@@ -1,73 +1,60 @@
-const section = document.querySelector('section');
-const container = document.createElement('div');
-container.setAttribute('id', 'grid');
+const container = document.querySelector('#container');
 container.style.height = '500px';
-section.appendChild(container);
 
+// Create initial grid with 16 squares per side
+let gridNumber = 16;
+const gridHeight = parseInt(container.style.height, 10) / gridNumber + 'px';
+console.log(gridHeight);
+let grid;
+for (let i = 0; i < gridNumber **2; i++) {
+    grid = document.createElement('div');
+    grid.style.height = gridHeight;
+    grid.setAttribute('class', 'grid');
+    container.appendChild(grid);
+}
 
-// Prompt user for number of grids
-// ANOTHER VERSOIN 
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>
+// Draw
+const drawing = document.querySelectorAll('.grid');
+drawing.forEach ((box) => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'hsl(280, 10%, 25%)';
+    })
+})
+
+// Listen for button
 document.querySelector('.button').addEventListener('click', () => {
-    document.querySelector('div').value = 0;
+// Delete previous grid
+    const removeGrid = document.getElementById("container");
+
+    while (removeGrid.hasChildNodes()) {
+    removeGrid.removeChild(removeGrid.firstChild);
+    }
+
+// Prompt user for number of squares
     do {
         promptNumber = parseInt(prompt("Number of squares per side (1 - 100): "), 10);
         console.log(promptNumber);
     }
     while (promptNumber < 1 || promptNumber >= 100 || isNaN(promptNumber)); 
     gridNumber = promptNumber;
-    // console.log(gridNumber);
+    console.log(gridNumber);
 
-    // return gridNumber;// Create grid of small squares
+// Make new grid
     const gridHeight = parseInt(container.style.height, 10) / gridNumber + 'px';
-    let row;
+    console.log(gridHeight);
+    let grid;
     for (let i = 0; i < gridNumber **2; i++) {
-        row = document.createElement('div');
-        row.style.height = gridHeight;
-        row.setAttribute('class', 'row');
-        container.appendChild(row);
+        grid = document.createElement('div');
+        grid.style.height = gridHeight;
+        grid.setAttribute('class', 'grid');
+        container.appendChild(grid);
     }
-    // Add querySelectorAll to draw on all grids
-    const drawing = document.querySelectorAll('.row');
+
+// Draw
+    const drawing = document.querySelectorAll('.grid');
     drawing.forEach ((box) => {
         box.addEventListener('mouseover', () => {
             box.style.backgroundColor = 'hsl(280, 10%, 25%)';
         })
     })
-});
-// location.reload();
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++->
-
-//____________________________________________________________________________________
-//WORKING WHILE LOOP
-//
-// let promptNumber = 16;
-
-// do {
-//     promptNumber = parseInt(prompt("Number of squares per side (1 - 100): "), 10);
-//     console.log(typeof promptNumber);
-// }
-//  while (promptNumber < 1 || promptNumber >= 100 || isNaN(promptNumber)); 
-
-// const gridNumber = promptNumber;
-// //-------------------------------------------------->
-
-// // Create grid of small squares
-// const gridHeight = parseInt(container.style.height, 10) / gridNumber + 'px';
-// let row;
-// for (let i = 0; i < gridNumber **2; i++) {
-//     row = document.createElement('div');
-//     row.style.height = gridHeight;
-//     // console.log(row.style.height);
-//     row.setAttribute('class', 'row');
-//     container.appendChild(row);
-// }
-
-// // Add querySelectorAll to draw on all grids
-// const drawing = document.querySelectorAll('.row');
-// drawing.forEach ((box) => {
-//     box.addEventListener('mouseover', () => {
-//         box.style.backgroundColor = 'hsl(280, 10%, 25%)';
-//     })
-// })
-//___________________________________________________________________________________
+})
