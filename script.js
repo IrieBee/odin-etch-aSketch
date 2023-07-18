ef8984846ad648ca4b110af4677782a2df01b967
itsy bitsy <119555750+ltsyBitsy@users.noreply.github.com>
2023-07-18 16:17:25 -0400

    Comment console.log

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ container.style.height = '500px';
 // Create initial grid with 16 squares per side
 let gridNumber = 16;
 const gridHeight = parseInt(container.style.height, 10) / gridNumber + 'px';
-console.log(gridHeight);
+// console.log(gridHeight);
 let grid;
 for (let i = 0; i < gridNumber **2; i++) {
     grid = document.createElement('div');
@@ -33,15 +33,15 @@ document.querySelector('.button').addEventListener('click', () => {
 // Prompt user for number of squares
     do {
         promptNumber = parseInt(prompt("Number of squares per side (1 - 100): "), 10);
-        console.log(promptNumber);
+        // console.log(promptNumber);
     }
     while (promptNumber < 1 || promptNumber >= 100 || isNaN(promptNumber)); 
     gridNumber = promptNumber;
-    console.log(gridNumber);
+    // console.log(gridNumber);
 
 // Make new grid
     const gridHeight = parseInt(container.style.height, 10) / gridNumber + 'px';
-    console.log(gridHeight);
+    // console.log(gridHeight);
     let grid;
     for (let i = 0; i < gridNumber **2; i++) {
         grid = document.createElement('div');
